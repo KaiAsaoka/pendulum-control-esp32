82af118d881c55163ea797f45a2edf8743026479
Andrew Du <andrew.selkirk17@gmail.com>
2025-03-22 16:24:41 -0700

    Fixed getMACAddress

--- src/getMACAddress.cpp
+++ src/getMACAddress.cpp
@@ -20,10 +20,7 @@ void begin(){
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
-<<<<<<< HEAD
-=======
   WiFi.begin();
->>>>>>> 3c1d6f40ec4bb8f38aeaf9bc19badc5d17594224
 
   Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
   readMacAddress();
