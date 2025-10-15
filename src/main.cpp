6115f316d6d46c19f08f88a7c7a3bb389dabb2ec
calmcy <113878718+calmcy@users.noreply.github.com>
2025-10-15 11:43:47 -0700

    changed D terms

--- src/main.cpp
+++ src/main.cpp
@@ -47,14 +47,14 @@ static volatile uint32_t overrun_count = 0;
 
 #define pendKPx 0.045
 #define pendKIx 0
-#define pendKDx 0
+#define pendKDx 0.0015
 
 #define pendlpfx 0
 #define pendintcutoffx (1000 / 0.018)
 
 #define pendKPy 0.015
 #define pendKIy 0
-#define pendKDy 0
+#define pendKDy 0.0015
 
 #define pendlpfy 0
 #define pendintcutoffy (2000 / 0.016)
