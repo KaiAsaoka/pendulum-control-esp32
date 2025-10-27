93ab7e20daef4fc1ea1fa9c311ad7eef15f34dfd
calmcy <113878718+calmcy@users.noreply.github.com>
2025-10-27 13:14:09 -0700

    change Ki

--- src/main.cpp
+++ src/main.cpp
@@ -46,14 +46,14 @@ static volatile uint32_t overrun_count = 0;
 #define SPEED 20
 
 #define pendKPx 0.045
-#define pendKIx 0
+#define pendKIx 0.15
 #define pendKDx 0.0005
 
 #define pendlpfx 0
 #define pendintcutoffx (1000 / 0.018)
 
 #define pendKPy 0.015
-#define pendKIy 0
+#define pendKIy 0.05
 #define pendKDy 0.00016
 
 #define pendlpfy 0
